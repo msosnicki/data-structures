7d6c9c641c4537bee84b8f3205848ba0ea5c9eab
msosnicki <msosnicki@ucs.imshealth.com>
2017-08-29 15:56:59 +0200

    Some further changes

--- app/week3/Problem2.hs
+++ app/week3/Problem2.hs
@@ -2,6 +2,7 @@ module Problem2 where
 
 import Control.Applicative
 import Data.Char
+import Data.Ord
 import Data.List
 import System.IO
 
@@ -9,30 +10,40 @@ import System.IO
 main :: IO ()
 main = do
   hSetBuffering stdin NoBuffering
-  putStrLn "tete"
+  n <- nextNum
+  m <- nextNum
+  nums <- nextNums m
+  putStrLn "tte"
+
+compareThread = comparing snd
+
+solve n jobs =
+  let pq = fromList compareThread $ zip [0..(n-1)] (repeat 0)
+  in scanl processJob pq jobs
 
 -- Heap related stuff
 type Rank = Int
+type Compare a = a -> a -> Ordering
 
 data Heap a = Tip | Node !Rank a (Heap a) (Heap a) deriving Show
 
 rank Tip = 0
 rank (Node r _ _ _) = r
 
-fromList :: Ord a => [a] -> Heap a
-fromList [] = Tip
-fromList (x:xs) = foldl' insertHeap (singleton x) xs
+fromList :: Compare a -> [a] -> Heap a
+fromList _ [] = Tip
+fromList cmp (x:xs) = foldl' (insertHeap cmp) (singleton x) xs
 
 singleton x = Node 1 x Tip Tip
 
-insertHeap h v = merge h $ singleton v
+insertHeap cmp h v = merge cmp h $ singleton v
 
-merge :: Ord a => Heap a -> Heap a -> Heap a
-merge Tip h = h
-merge h Tip = h
-merge h1@(Node _ x l1 r1) h2@(Node _ y l2 r2) =
-  if x<=y then makeHeap x l1 (merge r1 h2)
-  else makeHeap y l2 (merge h1 r2)
+merge :: Compare a -> Heap a -> Heap a -> Heap a
+merge _ Tip h = h
+merge _ h Tip = h
+merge cmp h1@(Node _ x l1 r1) h2@(Node _ y l2 r2) =
+  if cmp x y == GT then makeHeap x l1 (merge cmp r1 h2)
+  else makeHeap y l2 (merge cmp h1 r2)
 
 makeHeap x a b =
   if rank a >= rank b then Node (rank b + 1) x a b
@@ -41,8 +52,8 @@ makeHeap x a b =
 peekMin Tip = Nothing
 peekMin (Node _ x _ _) = x
 
-extractMin Tip = Nothing
-extractMin (Node _ x l r) = Just (x, merge l r)
+extractMin _ Tip = Nothing
+extractMin cmp (Node _ x l r) = Just (x, merge cmp l r)
 
 -- IO related stuff
 nextIOs io n = sequence $ replicate n io
